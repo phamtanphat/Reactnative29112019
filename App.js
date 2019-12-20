4eefeec83c6f983eaa05787f1f106120b1bb7f2d
phamtanphat <phatdroid94@gmail.com>
2019-12-20 03:47:24 -0800

    btap 1

--- App.js
+++ App.js
@@ -5,25 +5,25 @@ class App extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: '#abcef1',
-            flexDirection: 'column',
-            alignItems: 'flex-end',
-          }}>
-          <Text style={{backgroundColor: 'white'}}>View 1</Text>
-          <Text style={{backgroundColor: 'yellow'}}>View 2</Text>
+       <View style={styles.wrapper}>
+        <View style={styles.group1}>
+          <View style={styles.wrapText}>
+            <Text style={styles.textstyle}>A</Text>
+          </View>
+          <View style={styles.wrapText}>
+            <Text style={styles.textstyle}>B</Text>
+          </View>
+          <View style={styles.wrapText}>
+            <Text style={styles.textstyle}>C</Text>
+          </View>
+          <View style={styles.wrapText}>
+            <Text style={styles.textstyle}>D</Text>
+          </View>
         </View>
-        <View style={{flex: 1, backgroundColor: '#e30'}}>
-          <Text>View 2</Text>
+        <View style={styles.group2}>
+
         </View>
-        <View style={{flex: 1, backgroundColor: '#b40'}}>
-          <Text>View 3</Text>
-        </View>
-        <View style={{flex: 1, backgroundColor: '#a50'}}>
-          <Text>View 4</Text>
-        </View>
+       </View>
       </SafeAreaView>
     );
   }
@@ -32,6 +32,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+  },
+  wrapper: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  group1: {
+    backgroundColor: `rgb(255, 0, 255)`,
+    flex: 1,
+    flexDirection: 'column',
+  },
+  group2: {
+    backgroundColor: 'blue',
+    flex: 1,
+  },
+  textstyle: {
+    color: 'white',
+  },
+  wrapText: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems : 'center',
   },
 });
 export default App;
