3fe357591d7baaa2545448100d4aa2baebcca199
phamtanphat <phatdroid94@gmail.com>
2019-12-23 04:07:18 -0800

    dimension

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{flex: 1 , marginHorizontal :50}}>
           <Word />
         </View>
       </SafeAreaView>
--- src/Word.js
+++ src/Word.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {width , height} from './dimension';
 
 export default class Word extends Component {
   render() {
@@ -12,8 +13,8 @@ export default class Word extends Component {
           backgroundColor: 'lightgrey',
           justifyContent: 'space-around',
         }}>
-        <Text style={{fontSize: 30, color: '#27A745'}}> One </Text>
-        <Text style={{fontSize: 30, color: '#DC3545'}}> Mot </Text>
+        <Text style={{fontSize: width / 10, color: '#27A745'}}> One </Text>
+        <Text style={{fontSize: width / 10, color: '#DC3545'}}> Mot </Text>
       </View>
     );
   }
