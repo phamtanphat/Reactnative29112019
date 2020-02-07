f88ff714c50ac44e5bab4b76b0e3dde825fda4cd
phamtanphat <phatdroid94@gmail.com>
2020-02-07 03:42:33 -0800

    call api

--- src/List.js
+++ src/List.js
@@ -2,14 +2,30 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import {width} from './dimension';
+
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 import {connect} from 'react-redux';
 import FilterController from './Controller/FilterControl';
 import FormController from './Controller/FormController';
 import WordController from './Controller/WordController';
+import Response from './model/Reponse';
 
 class List extends Component {
+  componentDidMount() {
+    fetch('https://server2301.herokuapp.com/word', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        const response = new Response(data.success, data.words);
+        console.log(response.success);
+      });
+  }
   render() {
     return (
       <KeyboardAwareScrollView
--- src/model/WordModel.js
+++ src/model/WordModel.js
@@ -1,5 +1,5 @@
-export default function WordModel(id, en, vn, isMemorized = false) {
-  this.id = id;
+export default function WordModel(_id, en, vn, isMemorized = false) {
+  this._id = _id;
   this.en = en;
   this.vn = vn;
   this.isMemorized = isMemorized;
