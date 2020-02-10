201d15a693040069317e46f4da5a95f0bd651e8d
phamtanphat <phatdroid94@gmail.com>
2020-02-10 03:56:14 -0800

    stack navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import TextView from './src/TextView';
@@ -10,17 +11,41 @@ import Mymodal from './src/Mymodal';
 import Box from './src/Box';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Screen1 from './src/Navigation/Screen1';
+import Screen2 from './src/Navigation/Screen2';
+
+//App Todolist
+// class App extends Component {
+//   render() {
+//     return (
+//       <SafeAreaView style={{flex: 1}}>
+//         <View style={{flex: 1, marginHorizontal: width / 20}}>
+//           <Provider store={store}>
+//             <List />
+//           </Provider>
+//         </View>
+//       </SafeAreaView>
+//     );
+//   }
+// }
+
+// export default App;
+
+//Navigation
+
+const Stack = createStackNavigator();
 
 class App extends Component {
   render() {
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={{flex: 1, marginHorizontal: width / 20}}>
-          <Provider store={store}>
-            <List />
-          </Provider>
-        </View>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Screen1" component={Screen1} />
+          <Stack.Screen name="Screen2" component={Screen2} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
